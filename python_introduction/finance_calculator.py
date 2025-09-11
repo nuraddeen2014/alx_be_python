21cea85d95a16cc9f967413ae7c232ff508a7964
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-09-11 22:12:53 +0100

    remove int truncating

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,4 +5,4 @@ annual_interest_rate = 0.05
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Your monthly savings are: ${monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
+print(f"Projected savings after one year, with interest, is: ${projected_saving}.")
