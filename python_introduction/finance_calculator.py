0239d57f6df3fb044ac1723f4cd14847b8994853
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-09-11 22:23:23 +0100

    Variable naming error correction2

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 monthly_income = int(input("Enter your monthly income: "))
 monthly_expenses = int(input("Enter your monthly expenses: "))
-monthly_savings = (monthly_income - monthly_expenses 
+monthly_savings = (monthly_income - monthly_expenses)
 annual_interest_rate = 0.05
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
