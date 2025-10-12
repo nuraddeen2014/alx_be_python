89bbc56b0e8b68ef25d75deab7373a666502902b
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-10-12 11:47:45 +0100

    temp_conversion_tool corrected second

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -6,19 +6,17 @@ def convert_to_celsius(fahrenheit):
 
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR)+32
+try:
+    enter_temperature = int(input("Enter the temperature to convert: "))
+    celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
-enter_temperature = int(input("Enter the temperature to convert: "))
-celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
-
-match celsius_or_fahrenheit:
-    case 'C':
-        if type(enter_temperature) == str:
-            print("Invalid input")
-        else:
+    match celsius_or_fahrenheit:
+        case 'C':
             print(f"{enter_temperature}C is {convert_to_fahrenheit(enter_temperature)}")
-    case 'F':
-        if type(enter_temperature) == str:
-            print("Invalid input")
-        else:
+
+        case 'F':
             print(f"{enter_temperature}F is {convert_to_celsius(enter_temperature)}")
+
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.") 
         
