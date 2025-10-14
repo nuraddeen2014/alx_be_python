6088a07e4c0f90ce752763eaa7f6eba9accbb21e
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-10-14 21:05:30 +0100

    Modify the display to match regex

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,6 +13,6 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        return f"Current Balance: ${self.account_balance}"
+        return f"Current Balance: ${self.account_balance:.2f}"
     
 
