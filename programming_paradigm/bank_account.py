0cec9e29a0ed648f4cf1a9522760dc9690f83cdc
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-10-14 20:58:31 +0100

    add current balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,6 +13,6 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        return f"Account Balance: ${self.account_balance}"
+        return f"Current Balance: ${self.account_balance}"
     
 
