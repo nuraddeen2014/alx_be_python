b4fd1b6808c3d3bb0d26f44a8bdcbc0a17e515a4
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-10-15 07:20:10 +0100

    working with errors alx

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,19 @@
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+    except ValueError:
+        return "Please enter numeric values only."
+
+    else:
+        try:
+            if denominator == 0:
+                raise ZeroDivisionError
+            elif type(numerator)==str or type(denominator)==str:
+                raise ValueError
+            
+        except ZeroDivisionError:
+            return "Cannot divide by zero."
+        
+        else:
+            return f"the result of the division is {numerator/denominator}"
