4ed0135efe5bad2103b50399ad715507e1e1938f
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-10-18 04:34:14 +0100

    Advanced oop ALX

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,19 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplemented("Area not Implemented!")
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return f"{self.length*self.width}"
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi*self.radius
